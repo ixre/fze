3f89a681d163273e99eaafcc8571771714740721
jarrysix <jarrysix@gmail.com>
2020-05-10 11:10:58 +0800

    domain

---
+++ fze-commons/src/main/java/net/fze/arch/domain/IValueObject.java
@@ -0,0 +1,15 @@
+package net.fze.arch.domain;
+
+/**
+ * 值对象
+ */
+public interface IValueObject {
+    /**
+     * 值对象是否相同
+     *
+     * @param src    源对象
+     * @param target 目标对象
+     * @return 是或否
+     */
+    boolean equals(Object src, Object target);
+}
