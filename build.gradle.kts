4d4425b715a8590ad9f2fbcbd303e5574ea682e2
jarrysix <jarrysix@gmail.com>
2020-08-20 11:17:36 +0800

    build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+allprojects {
+    apply(plugin = "java")
+    group = "net.fze.arch"
+    version="0.1.77"
+}
+
+subprojects {
+    repositories {
+        maven("http://maven.aliyun.com/nexus/content/4/public/")       // manven
+        mavenCentral()
+    }
+
+    dependencies {
+        "implementation"(kotlin("stdlib"))
+        "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.5.1")
+        "testImplementation"("org.junit.jupiter:junit-jupiter-engine:5.5.1")
+    }
+}
