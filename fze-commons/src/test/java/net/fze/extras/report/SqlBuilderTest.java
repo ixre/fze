a62abb8b3d5901ab79facd69f61761f25784901e
jarrysix <jarrysix@gmail.com>
2023-03-07 15:11:57 +0800

    fix: 添加sql条件判断

--- fze-commons/src/test/java/net/fze/extras/report/SqlBuilderTest.java
+++ fze-commons/src/test/java/net/fze/extras/report/SqlBuilderTest.java
@@ -3,23 +3,38 @@ package net.fze.extras.report;
 import net.fze.jdk.jdk8.Maps;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 class SqlBuilderTest {
 
     @Test
     void resolve() {
-        String origin = "  SELECT * FROM order\n" +
-                "        WHERE {where}\n" +
-                " #if {status} \n"+
-                "   AND status = {status}\n" +
-                " #else\n"+
-                "   AND status <> 0"+
-                " #endif"+
-                "        ORDER BY {order_by}\n" +
-                "        LIMIT {page_offset},{page_size}";
+        String origin = "SELECT COUNT(1) FROM wal_wallet_log l \n" +
+                "\t\tWHERE l.wallet_id = {wallet_id}\\n AND title LIKE '%{keyword}%'\n" +
+                "\t\t#if { kind>0 }\n" +
+                "\t\t\tAND kind = {kind}\n" +
+                "\t\t#else\n" +
+                "\t\t\tAND kind = 'else'\n" +
+                "\t\t#fi\n" +
+                "\t\t#if { kind = 0 }\n" +
+                "\t\t\tAND kind = 0 + {kind}\n" +
+                "\t\t#fi\n" +
+                "\t\t#if {trade_no}\n" +
+                "\t\tAND\t(trade_no IS NULL OR outer_no LIKE '%{trade_no}%')\";\n" +
+                "\t\t#fi\n" +
+                "\t\t#if {check} AND (check = 1) #fi\n" +
+                "\n" +
+                "\t\t#if {unchecked} AND (uncheck = {kind}) #fi";
 
-       String sql1 =  SqlBuilder.resolve(origin, Maps.of("status","0.0"));
-       System.out.println(sql1);
-        String sql2 =  SqlBuilder.resolve(origin, Maps.of("status","False1"));
+        Map<String, Object> data = Maps.of("wallet_id", 0,
+                "keyword", "提现",
+                "kind", 0,
+                "trade_no", "F1",
+                "check", false,
+                "unchecked", true);
+        String sql1 = SqlBuilder.resolve(origin, data);
+        System.out.println(sql1);
+        String sql2 = SqlBuilder.resolve(origin, Maps.of("status", "False1"));
         System.out.println(sql2);
         System.out.println(0 == 0.0F);
     }
