842e593b62283f2b9ac5c17068f7684e2449a5f5
jarrysix <jarrysix@gmail.com>
2023-06-21 16:00:47 +0800

    fix:#AOXUEQI-3938 撤销对controller路由的更改

--- fze-commons/build.gradle.kts
+++ fze-commons/build.gradle.kts
@@ -7,16 +7,16 @@ apply("../deploy.gradle")
 
 
 dependencies {
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     //api("javax.xml.bind:jaxb-api:2.3.1")
     api("javax.inject:javax.inject:1")
-    api("com.google.code.gson:gson:2.9.1")
-    api("com.zaxxer:HikariCP:5.0.0")
     api("com.auth0:java-jwt:4.2.1")
-    implementation("org.aspectj:aspectjweaver:1.9.9")
+    compileOnly("com.zaxxer:HikariCP:5.0.1")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    compileOnly("com.google.code.gson:gson:2.10.1")
+    compileOnly("org.aspectj:aspectjweaver:1.9.19")
     compileOnly("javax.servlet:javax.servlet-api:4.0.1")
     compileOnly("javax.xml.bind:jaxb-api:2.3.1")
-    compileOnly("com.google.inject:guice:5.1.0")
+    compileOnly("com.google.inject:guice:7.0.0")
     compileOnly("com.moandjiezana.toml:toml4j:0.7.2")
     compileOnly("commons-cli:commons-cli:1.5.0")
     compileOnly("com.mchange:c3p0:0.9.5.5") //C3P0-Pool
@@ -24,7 +24,7 @@ dependencies {
     compileOnly("io.agroal:agroal-pool:1.13") // Agroal-Pool
     compileOnly("io.etcd:jetcd-core:0.5.11") //only jdk8
     compileOnly("redis.clients:jedis:4.2.3")
-    compileOnly("mysql:mysql-connector-java:8.0.27")
+    compileOnly("mysql:mysql-connector-java:8.0.28")
 
     //compileOnly("com.github.dozermapper:dozer-core:6.5.2")
 
@@ -32,10 +32,14 @@ dependencies {
     //compileOnly("com.esotericsoftware:reflectasm:1.11.9")
     // quarkus
     compileOnly("io.quarkus:quarkus-hibernate-orm-panache:1.8.0.Final")
+    // spring
+    compileOnly("com.baomidou:mybatis-plus-core:3.5.3")
+
+
     testImplementation("io.etcd:jetcd-core:0.5.4")
     compileOnly("org.junit.jupiter:junit-jupiter:5.7.0")
     testImplementation( "redis.clients:jedis:4.2.3")
-    implementation(kotlin("stdlib-jdk8"))
+    compileOnly(kotlin("stdlib-jdk8"))
 }
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
---
+++ fze-commons/src/main/java/net/fze/extras/mybatis/BaseJpaMapper.java
@@ -0,0 +1,42 @@
+package net.fze.extras.mybatis;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+/**
+ * 适配JPA规范的Mapper基础类型
+ * @param <P>
+ * @param <T>
+ */
+public interface BaseJpaMapper<P extends Serializable,T> extends BaseMapper<T> {
+    /** 根据主键查找 */
+    default Optional<T> findById(P id){
+        return Optional.of(this.selectById(id));
+    }
+
+    /** 保存实体 */
+    default T save(T e, Function<T,P> f){
+        Object pk = f.apply(e);
+        if(pk == null || pk.equals("") || pk.equals(0)) {
+            this.insert(e);
+        }else {
+            this.updateById(e);
+        }
+        return e;
+    }
+
+    /** 根据对象条件查找 */
+    default Optional<T> findOne(T o){
+        return Optional.of(this.selectOne(new QueryWrapper<>(o)));
+    }
+
+    /** 根据对象条件查找 */
+    default List<T> findAll(T o) {
+        return this.selectList(new QueryWrapper<>(o));
+    }
+}
