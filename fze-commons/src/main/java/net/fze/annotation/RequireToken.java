6f8392b8efcdef388c29c44689b18c13776d5285
jarrysix <jarrysix@gmail.com>
2023-10-25 14:52:56 +0800

    fix: 修复commons RequireToken未设置Runtime的错误

--- fze-commons/src/main/java/net/fze/annotation/RequireToken.java
+++ fze-commons/src/main/java/net/fze/annotation/RequireToken.java
@@ -1,14 +1,13 @@
 package net.fze.annotation;
 
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * 需要请求令牌,例如JWT
  */
 @Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME) // 需在Runtime中保持,不然拦劫时识别不了
 @Inherited
 public @interface RequireToken {
     /**
