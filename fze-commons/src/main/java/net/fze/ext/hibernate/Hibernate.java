a43840a6bdeac3a028ef0305cbf3381f21fde2c3
jarrysix <jarrysix@gmail.com>
2024-01-18 19:30:59 +0800

    Hibnerate.java

--- fze-commons/src/main/java/net/fze/ext/hibernate/Hibernate.java
+++ fze-commons/src/main/java/net/fze/ext/hibernate/Hibernate.java
@@ -6,7 +6,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.engine.transaction.jta.platform.internal.AtomikosJtaPlatform;
 
@@ -111,48 +110,85 @@ public class Hibernate {
 //                            (c) -> c.isAnnotationPresent(Entity.class)))
 //                    .forEach(configuration::addClass);
 //        }
+
+        // https://blog.csdn.net/weixin_34280237/article/details/91672332 Hibernate4.x
         // 读取配置文件信息
-        final StandardServiceRegistryBuilder builder =
-                new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties());
+//        final StandardServiceRegistryBuilder builder =
+//                new StandardServiceRegistryBuilder()
+//                        .applySettings(configuration.getProperties());
+
+//
+//        //  .configure(path);
+//        builder.applySetting("hibernate.connection.driver_class", _driverClass);
+//        builder.applySetting("hibernate.connection.url", _driverUrl);
+//        builder.applySetting("hibernate.connection.username", _dbUser);
+//        builder.applySetting("hibernate.connection.password", _dbPwd);
+//        builder.applySetting("hibernate.connection.autoReconnect", true);
+//        builder.applySetting("hibernate.connection.autoReconnectForPools", true);
+//        builder.applySetting("hibernate.connection.is-connection-validation-required", true);
+//        // 配置二级缓存
+//        builder.applySetting("hibernate.cache.use_second_level_cache", "false");
+//        builder.applySetting("hibernate.cache.use_query_cache", "false");
+//        builder.applySetting("hibernate.cache.provider_configuration_file_resource_path", "ehcache.xml");
+//        builder.applySetting("hibernate.cache.factory_class", "org.hibernate.cache.jcache.internal.JCacheRegionFactory");
+//        //builder.applySetting("hibernate.cache.provider_class","ehcache.xml");
+//
+//        // 配置JTA事务
+//        builder.applySetting("hibernate.transaction.jta.platform", AtomikosJtaPlatform.class.getName());
+//        builder.applySetting("javax.persistence.transactionType", "JTA");
+//        // 配置连接池
+//        builder.applySetting("hibernate.c3p0.max_statements", 0);
+//        builder.applySetting("hibernate.c3p0.acquire_increment", 3);
+//        builder.applySetting("hibernate.c3p0.testConnectionOnCheckin", true);
+//        builder.applySetting("hibernate.c3p0.testConnectionOnCheckout", false);
+//        builder.applySetting("hibernate.c3p0.idle_test_period", 3600);
+//        appendCustomSetting(builder, selfSettings);
+//        StandardServiceRegistry registry = builder.build();
+//        return new MetadataSources(registry)
+//                .buildMetadata()
+//                .buildSessionFactory();
+
+
         if(Strings.isNullOrEmpty(_confPath)) {
-            builder.configure();
+            configuration.configure();
         }else{
-            builder.configure("hibernate.cfg.xml");
+            configuration.configure("hibernate.cfg.xml");
         }
+
         //  .configure(path);
-        builder.applySetting("hibernate.connection.driver_class", _driverClass);
-        builder.applySetting("hibernate.connection.url", _driverUrl);
-        builder.applySetting("hibernate.connection.username", _dbUser);
-        builder.applySetting("hibernate.connection.password", _dbPwd);
-        builder.applySetting("hibernate.connection.autoReconnect", true);
-        builder.applySetting("hibernate.connection.autoReconnectForPools", true);
-        builder.applySetting("hibernate.connection.is-connection-validation-required", true);
+        configuration.setProperty("hibernate.connection.driver_class", _driverClass);
+        configuration.setProperty("hibernate.connection.url", _driverUrl);
+        configuration.setProperty("hibernate.connection.username", _dbUser);
+        configuration.setProperty("hibernate.connection.password", _dbPwd);
+        configuration.setProperty("hibernate.connection.autoReconnect", "true");
+        configuration.setProperty("hibernate.connection.autoReconnectForPools", "true");
+        configuration.setProperty("hibernate.connection.is-connection-validation-required", "true");
         // 配置二级缓存
-        builder.applySetting("hibernate.cache.use_second_level_cache", "false");
-        builder.applySetting("hibernate.cache.use_query_cache", "false");
-        builder.applySetting("hibernate.cache.provider_configuration_file_resource_path", "ehcache.xml");
-        builder.applySetting("hibernate.cache.factory_class", "org.hibernate.cache.jcache.internal.JCacheRegionFactory");
-        //builder.applySetting("hibernate.cache.provider_class","ehcache.xml");
+        configuration.setProperty("hibernate.cache.use_second_level_cache", "false");
+        configuration.setProperty("hibernate.cache.use_query_cache", "false");
+        configuration.setProperty("hibernate.cache.provider_configuration_file_resource_path", "ehcache.xml");
+        configuration.setProperty("hibernate.cache.factory_class", "org.hibernate.cache.jcache.internal.JCacheRegionFactory");
+        //configuration.setProperty("hibernate.cache.provider_class","ehcache.xml");
 
         // 配置JTA事务
-        builder.applySetting("hibernate.transaction.jta.platform", AtomikosJtaPlatform.class.getName());
-        builder.applySetting("javax.persistence.transactionType", "JTA");
+        configuration.setProperty("hibernate.transaction.jta.platform", AtomikosJtaPlatform.class.getName());
+        configuration.setProperty("javax.persistence.transactionType", "JTA");
         // 配置连接池
-        builder.applySetting("hibernate.c3p0.max_statements", 0);
-        builder.applySetting("hibernate.c3p0.acquire_increment", 3);
-        builder.applySetting("hibernate.c3p0.testConnectionOnCheckin", true);
-        builder.applySetting("hibernate.c3p0.testConnectionOnCheckout", false);
-        builder.applySetting("hibernate.c3p0.idle_test_period", 3600);
-        appendCustomSetting(builder, selfSettings);
-        StandardServiceRegistry registry = builder.build();
-        return new MetadataSources(registry)
-                .buildMetadata()
-                .buildSessionFactory();
+        configuration.setProperty("hibernate.c3p0.max_statements", "0");
+        configuration.setProperty("hibernate.c3p0.acquire_increment", "3");
+        configuration.setProperty("hibernate.c3p0.testConnectionOnCheckin", "true");
+        configuration.setProperty("hibernate.c3p0.testConnectionOnCheckout", "false");
+        configuration.setProperty("hibernate.c3p0.idle_test_period", "3600");
+        appendCustomSetting(configuration, selfSettings);
+        return configuration.buildSessionFactory();
+//        StandardServiceRegistry registry = builder.build();
+//        return new MetadataSources(registry)
+//                .buildMetadata()
+//                .buildSessionFactory();
     }
 
     private static void appendCustomSetting(
-            StandardServiceRegistryBuilder builder, Properties properties) throws NullPointerException {
+            Configuration builder, Properties properties) throws NullPointerException {
         if (properties != null) {
             for (Object key : properties.keySet()) {
                 if (!key.toString().startsWith("hibernate.")) {
@@ -162,7 +198,7 @@ public class Hibernate {
                     if (v == null) {
                         throw new NullPointerException("[ Hibernate][ Configure]: settings " + key + " value is null");
                     }
-                    builder.applySetting(key.toString(), v);
+                    builder.setProperty(key.toString(), v.toString());
                 }
             }
         }
